03894447ad2ae780e9f182901277452a48dba871
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-09-08 15:00:06 -0600

    server setup

---
+++ server.js
@@ -0,0 +1,27 @@
+// REQUIRE
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var sessionsModule = require('client-sessions');
+
+// EXPRESS APP
+var app = express();
+
+// CONNECT TO MONGODB
+mongoose.connect('mongodb://localhost/cigar_lounge', function(mongooseErr) {
+    if( mongooseErr ) { console.error(mongooseErr); }
+    else { console.info('Mongoose initialized!'); }
+});
+
+// BODY PARSER
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// STATIC
+app.use(express.static('./public'));
+
+
+
+// APP LISTEN
+app.listen(8080);
